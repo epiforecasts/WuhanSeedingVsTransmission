d324359a1cabaceedca8e83e792224a9ab2340f6
seabbs <s.e.abbott12@gmail.com>
2020-01-26 12:24:22 +0000

    added markdown output

--- inst/scripts/render_output.R
+++ inst/scripts/render_output.R
@@ -11,8 +11,13 @@ message("Rendering render to all other formats")
 ## Render to analysis to required output formats
 rmarkdown::render("output.Rmd", 
                   output_dir = "rendered_output", 
-                  knit_root_dir = ".",
+                  knit_root_dir = c("."),
                   output_format = c("html_document"))
+
+rmarkdown::render("output.Rmd", 
+                  output_dir = "rendered_output", 
+                  knit_root_dir = c("."),
+                  output_format = c("md_document"))
 
 message("Moving back into top level directory")
 setwd("..")
