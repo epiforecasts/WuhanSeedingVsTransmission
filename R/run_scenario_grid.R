bf88a061d508119ee035a7fcad4270498ccd8059
seabbs <s.e.abbott12@gmail.com>
2020-01-28 21:39:15 +0000

    added infrastructure to support custom serial interval sampling functions

--- R/run_scenario_grid.R
+++ R/run_scenario_grid.R
@@ -53,15 +53,21 @@ run_scenario_grid <- function(end_date = NULL, samples = 1, upper_case_bound = N
     event_size = c(20, 40, 60, 80, 100, 200, 400),
     event_duration = c(1, 7, 14, 21, 28),
     ## Serial mean (normal)
-    serial_mean = c(4, 6, 8.4, 12),
-    #8.4 from Lispsitch et al. (2003);  6, 4, 12 assumption driven
-    ## Uppper bound on the reproduction number
+    serial = list(tibble::tibble(
+      serial_type = c("MERS-like", "SARS-like", "initial SARS-like"),
+      serial_mean = c(6.8, 8.4, 10),
+      serial_sd = c(4.1, 3.8, 2.8),
+      serial_fn = c(rnorm, rnorm, rnorm)
+    )),
+    #From Lispsitch et al. (2003);  6, 4, 12 assumption driven
+    ## Bounds on the reproduction number
     ## Sampled from a uniform distribution 
     R0 = list(tibble::tibble(upper_R0 = c(1, 2, 3, 4),
-                        lower_R0 = c(0, 1, 2, 3))),
+                            lower_R0 = c(0, 1, 2, 3)))
   ) %>% 
     ## Add a scenario id
     tidyr::unnest("R0") %>% 
+    tidyr::unnest("serial") %>%
     dplyr::mutate(scenario = 1:dplyr::n())
   
   ## Sample paramters and set assumptions
@@ -69,7 +75,6 @@ run_scenario_grid <- function(end_date = NULL, samples = 1, upper_case_bound = N
     sample = 1:samples,
     ## Scenario analysis parameters
     ## Serial sd (normal)
-    serial_sd =  3.8, ## from Lispsitch et al. (2003)
     ## k
     k = 0.16, ## from Lloyd-Smith (2005).
     ##Outbreak length
--- R/run_sim.R
+++ R/run_sim.R
@@ -4,6 +4,9 @@
 #' @param n_length Numeric, the number of days in the initial seeding event
 #' @param mean_si Numeric, the mean of the serial interval
 #' @param sd_si Numeric, the sd of the serial interval
+#' @param serial_fn Function from which to sample the serial interval must 
+#' accepted a number of samples argument followed by a mean and standard deviation. If
+#' missing defaults to a normal distribution.
 #' @param R0 Numeric, the estimated reproduction number
 #' @param k Numeric, the dispersion of the negative binomial branching process
 #' @param tf Numeric, the end time of the branching process
@@ -26,7 +29,7 @@
 #'         
 #' ## Code
 #' run_sim
-run_sim  = function(n, n_length, mean_si, sd_si, R0, 
+run_sim  = function(n, n_length, mean_si, sd_si, serial_fn = NULL, R0, 
                     k=0.16, tf=37, kept_times = NULL, max_potential_cases,
                     delay_sample) {
   
@@ -34,10 +37,14 @@ run_sim  = function(n, n_length, mean_si, sd_si, R0,
     kept_times <- 0:tf
   }
   
+  if (is.null(serial_fn)) {
+    serial_fn <- function(x) {stats::rnorm(x, mean_si, sd_si)}
+  }
+  
   t0 <- sample(seq(1, n_length), n, replace = TRUE)  
 
   ## simulate chains
-  sim <- bpmodels::chain_sim(n, "nbinom", serial = function(x) stats::rnorm(x, mean_si, sd_si),
+  sim <- bpmodels::chain_sim(n, "nbinom", serial = serial_fn,
                    mu = R0, size = k, t0 = t0, tf = tf, infinite = max_potential_cases)
   
   ## add reporting delays
